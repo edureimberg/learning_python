e30563489297f1d3e1e48678b08d08dcb92ec0ba
Eduardo <eduardo.reimberg@gmail.com>
2016-02-19 12:04:16 -0200

    Criação do arquivo multiple.sequences.py

---
+++ multiple.sequences.py
@@ -0,0 +1,7 @@
+people = ['Jonas','Julio','Mike','Mez']
+ages = ['25','20','31','39']
+
+for position in range(len(people)):
+    person = people[position]
+    age = ages[position]
+    print(person, age)
