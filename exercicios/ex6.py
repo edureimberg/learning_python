34f8a2befdf6506fd51879df0cf0fe3b7d17327a
Eduardo <eduardo.reimberg@gmail.com>
2016-03-02 19:05:44 -0300

    Exercício 6

---
+++ exercicios/ex6.py
@@ -0,0 +1,6 @@
+print("Programa para calcular o tempo de viagem")
+
+distancia = int(input("Digite a distancia a ser percorrida (KM):"))
+velocidade = int(input("Digite a velocidade do veículo (KM/H):"))
+
+print("O tempo da viagem é:", (distancia * 1) / velocidade, "horas")
