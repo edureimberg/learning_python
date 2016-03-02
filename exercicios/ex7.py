449d042b60b3f87d71fa5c8d99c9d1a4331e0ed2
Eduardo <eduardo.reimberg@gmail.com>
2016-03-02 19:22:41 -0300

    Exercício 7

---
+++ exercicios/ex7.py
@@ -0,0 +1,5 @@
+print("Programa que converte temperaturas em Celsius para Fahrenheit")
+
+celsius = int(input("Digite a temperatura que será convertida: "))
+
+print("A temperatura convertida em Fahrenheit é:", ((9 * celsius) / 5) + 32)
