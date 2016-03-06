5f3428c44f87b6270ae6c774cf008a06b34b2402
Eduardo <eduardo.reimberg@gmail.com>
2016-03-06 12:36:04 -0300

    Criação do arquivo pythagorean.triple.comprehension.py

---
+++ pythagorean.triple.comprehension.py
@@ -0,0 +1,9 @@
+from math import sqrt
+
+mx = 10
+
+legs = [(a, b, sqrt(a ** 2 + b ** 2)) for a in range(1, mx) for b in range(a, mx)]
+
+legs = [(a, b, int(c)) for a, b, c in legs if c.is_integer()]
+
+print(legs)
