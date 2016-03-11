47473b9916c598c05a548c9b8e8dceccee5482eb
Eduardo <eduardo.reimberg@gmail.com>
2016-03-10 21:18:27 -0300

    Adição do modo comprehension

--- dictionary.comprehensions.py
+++ dictionary.comprehensions.py
@@ -4,3 +4,8 @@ lettermap = dict((c, k) for k, c in enumerate(ascii_lowercase, 1))
 
 print(lettermap)
 print(len(lettermap))
+
+#Versão com comprehension
+
+lettermap2 = {c: k for k, c in enumerate(ascii_lowercase, 1)}
+print(lettermap2)
