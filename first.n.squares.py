e2da1078d0336306c18be920745753d477a4518a
Eduardo <eduardo.reimberg@gmail.com>
2016-03-10 21:37:38 -0300

    Criação do arquivo first.n.squares.py

---
+++ first.n.squares.py
@@ -0,0 +1,12 @@
+#função clássica
+def get_squares(n):
+    return [x ** 2 for x in range(n)]
+
+print(get_squares(10))
+
+#Generator
+def get_squares_gen(n):
+    for x in range(n):
+        yield x ** 2
+
+print(list(get_squares_gen(10)))
